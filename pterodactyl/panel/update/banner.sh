40b0aacd0b1288dbab6caa6dddfa1a235e002e21
ks_warrior <kswarriorpro@gmail.com>
2025-09-25 15:41:10 +0530

    Create banner.sh

---
+++ pterodactyl/panel/update/banner.sh
@@ -0,0 +1,12 @@
+banner() {
+    clear
+    BLUE='\033[1;34m'
+    GREEN='\033[1;32m'
+    YELLOW='\033[1;33m'
+    NC='\033[0m'
+
+    echo -e "${BLUE}╔════════════════════════════════╗"
+    echo -e "║ ${YELLOW}🚀 Updateing ${GREEN}Pterodactyl Panel${BLUE} ║"
+    echo -e "╚════════════════════════════════╝${NC}"
+}
+banner
